aef398c044b7bddf97773d438939ff33c4df49a6
Vivek-Indi <vivekindi007@gmail.com>
2021-06-08 00:50:02 +0530

    Update settings.py

--- lab_exam/settings.py
+++ lab_exam/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'lab_exam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'assignment',
@@ -97,7 +97,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
-}
+}'''
 
 
 # Password validation
